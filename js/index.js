658d15873ff1fec0ccf331d1a3150ec26e020f88
If <247596814@qq.com>
2017-08-01 23:09:34 +0800

    update 修改url

--- js/index.js
+++ js/index.js
@@ -1,198 +1,198 @@
 window.onload = function () {
     var list = [{
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/001.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/001.jpg",
             title: "海报"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/002.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/002.jpg",
             title: "海报"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/003.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/003.jpg",
             title: "海报"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/004.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/004.jpg",
             title: "海报"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/005.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/005.jpg",
             title: "海报"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/006.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/006.jpg",
             title: "海报"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/007.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/007.jpg",
             title: "海报"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/008.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/008.jpg",
             title: "海报"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/009.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/009.jpg",
             title: "海报"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/010.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/010.jpg",
             title: "海报"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/011.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/011.jpg",
             title: "海报"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/012.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/012.jpg",
             title: "海报"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/013.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/013.jpg",
             title: "海报"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/014.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/014.jpg",
             title: "海报"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/015.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/015.jpg",
             title: "海报"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/016.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/016.jpg",
             title: "海报"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/017.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/017.jpg",
             title: "海报"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/018.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/018.jpg",
             title: "海报"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/019.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/019.jpg",
             title: "海报"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/020.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/020.jpg",
             title: "红酒展架"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/021.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/021.jpg",
             title: "红酒展架"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/022.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/022.jpg",
             title: "红酒展架"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/023.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/023.jpg",
             title: "红酒展架"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/024.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/024.jpg",
             title: "红酒展架"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/025.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/025.jpg",
             title: "红酒展架"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/026.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/026.jpg",
             title: "红酒展架"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/027.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/027.jpg",
             title: "红酒展架"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/028.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/028.jpg",
             title: "红酒展架"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/029.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/029.jpg",
             title: "红酒展架"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/030.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/030.jpg",
             title: "红酒展架"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/031.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/031.jpg",
             title: "红酒展架"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/032.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/032.jpg",
             title: "产品详情页"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/033.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/033.jpg",
             title: "产品详情页"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/034.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/034.jpg",
             title: "产品详情页"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/035.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/035.jpg",
             title: "产品详情页"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/036.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/036.jpg",
             title: "产品详情页"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/037.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/037.jpg",
             title: "产品详情页"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/038.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/038.jpg",
             title: "产品详情页"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/039.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/039.jpg",
             title: "产品详情页"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/040.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/040.jpg",
             title: "产品详情页"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/041.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/041.jpg",
             title: "产品详情页"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/042.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/042.jpg",
             title: "产品详情页"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/043.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/043.jpg",
             title: "产品详情页"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/044.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/044.jpg",
             title: "产品详情页"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/045.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/045.jpg",
             title: "产品详情页"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/046.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/046.jpg",
             title: "产品详情页"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/047.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/047.jpg",
             title: "产品详情页"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/048.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/048.jpg",
             title: "产品详情页"
         },
         {
-            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/049.jpg",
+            "url": "http://zaw001-1254097928.cosgz.myqcloud.com/049.jpg",
             title: "产品详情页"
         }
     ];
