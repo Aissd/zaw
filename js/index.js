cbed9a4a4b74e5df30272ed57ce6d8e0b0234359
If <247596814@qq.com>
2017-08-01 19:46:23 +0800

    20170801-1946:更换瀑布流插件

--- js/index.js
+++ js/index.js
@@ -1,228 +1,211 @@
-$(function(){
-    var imgArray = [
-        {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/001.jpg",
-            "title": "jQuery瀑布流特效"
+$(document).ready(function () {
+    $(".mygallery").chromaGallery({
+        color: '#000',
+        gridMargin: 15,
+        maxColumns: 5,
+        dof: true,
+        screenOpacity: 0.8
+    });
+});
+
+window.onload = function () {
+    var list = [{
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/001.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/002.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/002.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/003.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/003.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/004.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/004.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/005.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/005.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/006.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/006.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/007.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/007.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/008.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/008.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/009.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/009.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/010.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/010.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/011.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/011.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/012.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/012.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/013.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/013.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/014.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/014.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/015.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/015.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/016.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/016.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/017.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/017.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/018.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/018.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/019.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/019.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/020.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/020.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/021.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/021.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/022.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/022.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/023.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/023.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/024.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/024.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/025.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/025.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/026.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/026.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/027.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/027.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/028.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/028.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/029.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/029.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/030.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/030.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/031.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/031.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/032.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/032.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/033.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/033.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/034.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/034.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/035.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/035.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/036.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/036.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/037.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/037.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/038.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/038.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/039.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/039.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/040.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/040.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/041.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/041.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/042.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/042.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/043.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/043.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/044.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/044.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/045.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/045.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/046.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/046.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/047.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/047.jpg",
+            title: ""
         },
         {
-            "url": "http://zaw-1254097928.cosgz.myqcloud.com/048.jpg",
-            "title": "jQuery瀑布流特效"
+            "url": "http://zaw01-1254097928.cosgz.myqcloud.com/048.jpg",
+            title: ""
         }
     ];
-    var html = "";
-    for(var i = 0, len = imgArray.length; i < len; i++){
-        html += "<div class='cell'>" + 
-                    "<a href='javascript:;'>" + 
-                    "<img src='" + imgArray[i].url + "' /></a>" + 
-                    // "<p>" + 
-                    //     "<a href='javascript:;'>" + imgArray[i].title + "</a>" + 
-                    // "</p>" + 
-                "</div>'";
+
+    var html = '';
+    for (var i = 0, len = list.length; i < len; i++) {
+        html += "<img src='" + list[i].url + "' alt='" + list[i].title + "' data-largesrc='" + list[i].url + "'>";
     }
-    $('#waterfall').append(html);
-    // 瀑布流初始化
-    var opt = {
-        getResource: function (index, render) { 
-            //index为已加载次数,render为渲染接口函数,接受一个dom集合或jquery对象作为参数。通过ajax等异步方法得到的数据可以传入该接口进行渲染，如 render(elem)
-            if (index >= 7) index = index % 7 + 1;
-            var html = '';
-            for (var i = 20 * (index - 1); i < 20 * (index - 1) + 20; i++) {
-                var k = '';
-                for (var ii = 0; ii < 3 - i.toString().length; ii++) {
-                    k += '0';
-                }
-                k += i;
-                // var src = "http://cued.xunlei.com/demos/publ/img/P_" + k + ".jpg";
-                // html += '<div class="cell"><a href="#"><img src="' + src + '" /></a><p>' + k + '</p></div>';
-            }
-            return $(html);
-        },
-        auto_imgHeight: true,
-        insert_type: 1
-    }
-    $('#waterfall').waterfall(opt);
-});
+    $("#mygallery").append(html);
+};
