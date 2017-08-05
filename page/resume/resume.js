bab9e72e80ee16bf298d69370c553dcce570e1b5
If <247596814@qq.com>
2017-08-05 11:29:41 +0800

    20170805-1129:更改个人简历，换成全屏切换模式

---
+++ page/resume/resume.js
@@ -0,0 +1,11 @@
+$(function(){
+	$('#dowebok').fullpage({
+        menu: '#archor-menu',
+		anchors: ['page1', 'page2', 'page3', 'page4'],
+        sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', '#f90'],
+        afterLoad: function(){
+            
+        }
+    });
+    
+});
